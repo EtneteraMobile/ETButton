50e601bd36b57ae173ca75fa046b8e60688183ce
Jiri Zoudun <jiri.zoudun@etnetera.cz>
2018-08-17 16:43:45 +0200

    State tests (master)

---
+++ Tests/ETButtonTests/Constants.swift
@@ -0,0 +1,24 @@
+//
+//  Constants.swift
+//  ETButton-iOS Tests
+//
+//  Created by Jiří Zoudun on 17/08/2018.
+//  Copyright © 2018 Etnetera a.s. All rights reserved.
+//
+
+import UIKit
+
+class Constants {
+    static let backgroundColor = UIColor(red: 0.133, green: 0.407, blue: 0.121, alpha: 1)
+    static let highlightedBackgroundColor = UIColor(red: 0.133 - 0.1, green: 0.407 - 0.1, blue: 0.121 - 0.1, alpha: 1)
+    static let selectedBackgroundColor = UIColor(red: 0.7, green: 1, blue: 0.7, alpha: 1)
+    static let disabledBackgroundColor = UIColor(red: 0.4, green: 0.4, blue: 0.4, alpha: 1)
+
+    static let foregroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+    static let highlightedForegroundColor = UIColor(red: 0.5, green: 0, blue: 0, alpha: 1)
+    static let selectedForegroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+    static let disabledForegroundColor = UIColor(red: 0.1, green: 0.1, blue: 0.1, alpha: 1)
+
+    static let boldFont = UIFont.boldSystemFont(ofSize: 14)
+    static let italicFont = UIFont.italicSystemFont(ofSize: 14)
+}
--- Tests/ETButtonTests/ETButtonTests.swift
+++ Tests/ETButtonTests/ETButtonTests.swift
@@ -9,15 +9,119 @@
 import Foundation
 import XCTest
 import ETButton
+import FBSnapshotTestCase
 
-class ETButtonTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        //// XCTAssertEqual(ETButton().text, "Hello, World!")
+class StateSnapshotTests: FBSnapshotTestCase {
+
+    var button: Button!
+    var customStyle: Button.Style!
+
+    override func setUp() {
+        super.setUp()
+
+        button = Button(frame: CGRect(x: 0, y: 0, width: 200, height: 44))
+        customStyle = Button.Style(backgroundColor: Constants.backgroundColor,
+                                    backgroundHighlightedColor: Constants.highlightedBackgroundColor,
+                                    backgroundSelectedColor: Constants.selectedBackgroundColor,
+                                    backgroundDisabledColor: Constants.disabledBackgroundColor,
+                                    titleStyle: Button.LabelStyle(textColor: Constants.foregroundColor, font: Constants.boldFont),
+                                    titleHighlightedStyle: Button.LabelStyle(textColor: Constants.highlightedForegroundColor),
+                                    titleSelectedStyle: Button.LabelStyle(textColor: Constants.selectedForegroundColor),
+                                    titleDisabledStyle: Button.LabelStyle(textColor: Constants.disabledForegroundColor, font: Constants.italicFont))
+
+        recordMode = false
     }
 
-    static var allTests = [
-        ("testExample", testExample)
-    ]
+    func testNoneButtonNormalState() {
+        button.style = .none
+        button.setTitle("none", for: .normal)
+
+        FBSnapshotVerifyView(button)
+    }
+
+    func testNoneButtonHighlightedState() {
+        button.style = .none
+        button.setTitle("none", for: .normal)
+        button.isHighlighted = true
+
+        FBSnapshotVerifyView(button)
+    }
+
+    func testNoneButtonSelectedState() {
+        button.style = .none
+        button.setTitle("none", for: .normal)
+        button.isSelected = true
+
+        FBSnapshotVerifyView(button)
+    }
+
+    func testNoneButtonDisabledState() {
+        button.style = .none
+        button.setTitle("none", for: .normal)
+        button.isEnabled = false
+
+        FBSnapshotVerifyView(button)
+    }
+
+    func testBasicButtonNormalState() {
+        button.style = .basic
+        button.setTitle("basic", for: .normal)
+
+        FBSnapshotVerifyView(button)
+    }
+
+    func testBasicButtonHighlightedState() {
+        button.style = .basic
+        button.setTitle("basic", for: .normal)
+        button.isHighlighted = true
+
+        FBSnapshotVerifyView(button)
+    }
+
+    func testBasicButtonSelectedState() {
+        button.style = .basic
+        button.setTitle("basic", for: .normal)
+        button.isSelected = true
+
+        FBSnapshotVerifyView(button)
+    }
+
+    func testBasicButtonDisabledState() {
+        button.style = .basic
+        button.setTitle("basic", for: .normal)
+        button.isEnabled = false
+
+        FBSnapshotVerifyView(button)
+    }
+
+    func testCustomButtonNormalState() {
+        button.style = customStyle
+        button.setTitle("custom", for: .normal)
+
+        FBSnapshotVerifyView(button)
+    }
+
+    func testCustomButtonHighlightedState() {
+        button.style = customStyle
+        button.setTitle("custom", for: .normal)
+        button.isHighlighted = true
+
+        FBSnapshotVerifyView(button)
+    }
+
+    func testCustomButtonSelectedState() {
+        button.style = customStyle
+        button.setTitle("custom", for: .normal)
+        button.isSelected = true
+
+        FBSnapshotVerifyView(button)
+    }
+
+    func testCustomButtonDisabledState() {
+        button.style = customStyle
+        button.setTitle("custom", for: .normal)
+        button.isEnabled = false
+
+        FBSnapshotVerifyView(button)
+    }
 }
